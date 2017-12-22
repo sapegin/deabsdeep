0c3d8d3a8b28021d1a6e966f13c085b1ae32e1ef
Artem Sapegin <artem@sapegin.ru>
2017-12-22 14:03:59 +0100

    Fix: Node 4 compat

--- src/getRootDir.js
+++ src/getRootDir.js
@@ -6,7 +6,8 @@ const path = require('path');
  * @param {string} [dir]
  * @return {string}
  */
-module.exports = function gtRootDir(dir = __dirname) {
+module.exports = function gtRootDir(dir) {
+	dir = dir || __dirname;
 	const m = dir.match(/[\\/]node_modules[\\/]/);
 	return m ? dir.substring(0, m.index) : path.resolve(require.resolve('.'), '../..');
 };
